54abc344e2b2d991bf0723538d1f3ad820e435b0
Kuba <silverzus465@gmail.com>
2019-07-09 21:57:19 +0200

    -fix for the profile tab

--- src/AppBundle/Controller/Web/ProfileController.php
+++ src/AppBundle/Controller/Web/ProfileController.php
@@ -22,12 +22,14 @@ class ProfileController extends Controller
     public function showAction(UserStatsService $userStatsService)
     {
         $user = $this->getUser();
+        $userId= $user->getId();
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
 
         return $this->render('pages/profile.html.twig', array(
             'user' => $user,
+            'userId' => $userId,
             'puzzlesSolved' => $userStatsService->getSolvedPuzzlesCombined($user),
         ));
      }
